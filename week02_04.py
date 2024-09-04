338f99e80d02b8bc65f533476eb5991192e0d45d
sldjfrnfwltkdfuf <163097225+sldjfrnfwltkdfuf@users.noreply.github.com>
2024-09-04 12:15:43 +0900

    Add files via upload

---
+++ week02_04.py
@@ -0,0 +1,34 @@
+#week02_04.py
+
+a=int(5)
+b=int(2)
+c=float(2.4)
+
+div1=a/b
+div2=a//b
+div3=a%b
+div4=c/b
+div5=c//b
+div6=c%b
+print(div1,div2,div3)
+print(div4,div5,div6)
+
+sqr1=a**b
+sqr2=b**c
+print(sqr1,sqr2)
+
+add=a + b
+sub=b - c
+print(add,sub)
+
+mul1=a * b
+mul2=a * c
+print(mul1,mul2)
+
+
+decimal =int(13)
+binary = int(0b1101)
+octal= int(0o15)
+hexadecimal=int (0xD)
+
+print(decimal,binary,octal,hexadecimal)
